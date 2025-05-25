67b06de26fff976130f106307d63704d3ab7d370
lizz <home@lizz.dev>
2025-05-25 08:32:47 +0300

    Add simple debugging support

--- index.ts
+++ index.ts
@@ -12,3 +12,14 @@ const envSchema = z.object({
 console.log(process.env.MY_KEY);
 const parsed = envSchema.parse(process.env);
 console.log("PARSED: ", parsed);
+
+let num = 5;
+num++;
+
+function add(a: number, b: number) {
+  const test = 1;
+  return a + b + test;
+}
+
+add(num, num);
+console.log(num);
